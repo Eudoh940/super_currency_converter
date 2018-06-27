e550848ddb1c89731e77894537b73d0ebe472981
achiengcindy <achiengcindy36@gmail.com>
2018-06-27 17:06:18 +0300

    cteating html

---
+++ js/app.js
@@ -0,0 +1,33 @@
+/*var converter = CurrencyConverter({
+    API : { 
+        url: 'https://free.currencyconverterapi.com/',
+    }
+});
+const endpoint = 'https://free.currencyconverterapi.com/';*/
+
+function loadCurrency()
+{
+    /*const xmlHttp = null;
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false );
+    xmlHttp.send( null );
+    return xmlHttp.responseText;*/
+    const xmlHttp = new XMLHttpRequest();
+    xHttp.onreadystatechange = function(){
+        if (xHttp.readystate ==  && xHttp )
+    }
+}
+
+function convertCurrency(){
+    const from-documentById('from').value;
+    const from-documentById('to').value;
+    const from-documentById('currency').value;
+
+
+    if (from.length>0 && to.length>0 && currency.length>0) {
+        const xHttp = new XMLHttpRequest();
+        Http.open('GET','https://free.currencyconverterapi.com/', true);
+        xHttp.send();
+    }
+}
+
